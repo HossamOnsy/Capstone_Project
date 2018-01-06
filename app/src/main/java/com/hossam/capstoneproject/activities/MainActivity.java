b385f08a211fd029863ce2d6dd86fb0d1fbb4a38
Hossam.Onsy <Hossam.Onsy@Orchtech.com>
2018-01-06 18:29:33 +0200

    First 2 Cores are on point

---
+++ app/src/main/java/com/hossam/capstoneproject/activities/MainActivity.java
@@ -0,0 +1,306 @@
+package com.hossam.capstoneproject.activities;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnPausedListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.hossam.capstoneproject.R;
+import com.hossam.capstoneproject.utils.FileUtils;
+
+import java.net.URISyntaxException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int FILE_SELECT_CODE = 0;
+    private static final int READ_EXTERNAL_STORAGE_PERMISSION_CODE = 101;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    @BindView(R.id.progressbar)
+    ProgressBar progressBar;
+    @BindView(R.id.upload)
+    Button mUploadButton;
+    @BindView(R.id.video_view)
+    SimpleExoPlayerView video_view;
+
+    SimpleExoPlayer player;
+
+    Bundle bundle = null;
+    boolean isPlayWhenReady =true;
+
+    Unbinder unbinder;
+
+    @OnClick({R.id.upload})
+    void View(View view) {
+        switch (view.getId()) {
+            case R.id.upload: {
+                mUploadButton.setClickable(false);
+                progressBar.setVisibility(View.VISIBLE);
+                checkPermissions();
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        unbinder = ButterKnife.bind(this);
+    }
+
+    void checkPermissions() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+
+            // No explanation needed, we can request the permission.
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CONTACTS},
+                    READ_EXTERNAL_STORAGE_PERMISSION_CODE);
+
+            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+
+        } else
+            showFileChooser();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case READ_EXTERNAL_STORAGE_PERMISSION_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    showFileChooser();
+                } else {
+                    Toast.makeText(this, "Please Allow Reading From External Storage For Uploading", Toast.LENGTH_SHORT).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        try {
+            startActivityForResult(
+                    Intent.createChooser(intent, "Select a File to Upload"),
+                    FILE_SELECT_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(this, "Please install a File Manager.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case FILE_SELECT_CODE:
+                if (resultCode == RESULT_OK) {
+                    // Get the Uri of the selected file
+                    Uri uri = data.getData();
+                    Log.d(TAG, "File Uri: " + uri.toString());
+                    checkAuth(uri);
+                    // Get the path
+                    String path = null;
+                    try {
+                        path = FileUtils.getPath(this, uri);
+                        Log.d(TAG, "File Path: " + path);
+                    } catch (URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+
+                    // Get the file instance
+                    // File file = new File(path);
+                    // Initiate the upload
+                }
+                mUploadButton.setClickable(true);
+                progressBar.setVisibility(View.GONE);
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void checkAuth(final Uri uri) {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            uploadFileToFirebase(uri);
+        } else {
+            mAuth.signInAnonymously().addOnSuccessListener(this, new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    uploadFileToFirebase(uri);
+                }
+            }).addOnFailureListener(this, new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    Log.d(TAG, "signInAnonymously:FAILURE", exception);
+                }
+            });
+        }
+    }
+
+
+    private void uploadFileToFirebase(Uri uri) {
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        // Create a storage reference from our app
+        StorageReference storageRef = storage.getReference();
+
+        // File or Blob   //uri
+
+        // Create the file metadata
+        StorageMetadata metadata = new StorageMetadata.Builder()
+                .setContentType("audio/mpeg")
+                .build();
+        // Upload file and metadata to the path 'images/mountains.jpg'
+        UploadTask uploadTask = storageRef.child("songs/" + uri.getLastPathSegment()).putFile(uri, metadata);
+
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                System.out.println("Upload is " + progress + "% done");
+                Log.d(TAG, "taskSnapshot -----> " + "Upload is " + progress + "% done" + " <----------");
+                progressBar.setVisibility(View.VISIBLE);
+            }
+        }).addOnPausedListener(new OnPausedListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onPaused(UploadTask.TaskSnapshot taskSnapshot) {
+                System.out.println("Upload is paused");
+                Log.d(TAG, "taskSnapshot -----> " + "Upload is paused" + " <----------");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+                Log.d(TAG, "exception -----> " + exception.getMessage() + " <----------");
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // Handle successful uploads on complete
+                Uri downloadUrl = taskSnapshot.getMetadata().getDownloadUrl();
+                initializePlayer(downloadUrl);
+                Log.d(TAG, "taskSnapshot -----> " + downloadUrl + " <----------");
+                Log.d(TAG, "taskSnapshot -----> " + taskSnapshot.getMetadata().getName() + " <----------");
+            }
+        });
+
+    }
+
+
+    private void initializePlayer(Uri uri) {
+        if (uri != null) {
+//            SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(
+//                    new DefaultRenderersFactory(getActivity().getApplicationContext()),
+//                    new DefaultTrackSelector(), new DefaultLoadControl());
+
+            MediaSource mediaSource = buildMediaSource(uri);
+            if (player == null) {
+                player = ExoPlayerFactory.newSimpleInstance(
+                        new DefaultRenderersFactory(this.getApplicationContext()),
+                        new DefaultTrackSelector(), new DefaultLoadControl());
+
+            }
+            if(bundle!=null)
+                isPlayWhenReady = bundle.getBoolean("playstate");
+            player.setPlayWhenReady(isPlayWhenReady);
+            player.prepare(mediaSource, true, false);
+
+            video_view.setPlayer(player);
+            if (bundle != null) {
+                player.seekTo(bundle.getLong("timer"));
+
+            }
+
+        }
+
+//        player.setPlayWhenReady(playWhenReady);
+//        player.seekTo(currentWindow, playbackPosition);
+    }
+
+
+    private MediaSource buildMediaSource(Uri uri) {
+        return new ExtractorMediaSource(uri,
+                new DefaultHttpDataSourceFactory("ua"),
+                new DefaultExtractorsFactory(), null, null);
+    }
+//    @Override
+//    public void onPause() {
+//        super.onPause();
+//        if (player != null) {
+//            player.release();
+//        }
+//    }
+
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//        if (player != null) {
+//            player.release();
+//        }
+//    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (unbinder != null)
+            unbinder.unbind();
+    }
+}
---
+++ app/src/main/java/com/hossam/capstoneproject/activities/SplashActivity.java
@@ -0,0 +1,237 @@
+package com.hossam.capstoneproject.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.hossam.capstoneproject.R;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+public class SplashActivity extends AppCompatActivity implements RecognitionListener {
+    private static final String TAG = SplashActivity.class.getSimpleName();
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    SpeechRecognizer speech;
+    @BindView(R.id.intriguing)
+    SwitchCompat intriguing;
+    @BindView(R.id.say_ok_random_text_view)
+    TextView say_ok_random_text_view;
+    @BindView(R.id.progressBar1)
+    ProgressBar progressBar1;
+
+
+    Unbinder unbinder;
+
+    public String getErrorText(int errorCode) {
+        String message;
+        switch (errorCode) {
+            case SpeechRecognizer.ERROR_AUDIO:
+                message = "Audio recording error";
+                break;
+            case SpeechRecognizer.ERROR_CLIENT:
+                message = "Client side error";
+                break;
+            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                message = "Insufficient permissions";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK:
+                message = "Network error";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                message = "Network timeout";
+                break;
+            case SpeechRecognizer.ERROR_NO_MATCH:
+                message = "No match";
+                promptSpeechInput();
+                break;
+            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                message = "RecognitionService busy";
+                break;
+            case SpeechRecognizer.ERROR_SERVER:
+                message = "error from server";
+                break;
+            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                message = "No speech input";
+                break;
+            default:
+                message = "Didn't understand, please try again.";
+                break;
+        }
+        return message;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        unbinder = ButterKnife.bind(this);
+
+
+    }
+
+    @OnClick({R.id.intriguing})
+    void View(View view) {
+        switch (view.getId()) {
+            case R.id.intriguing: {
+                say_ok_random_text_view.setVisibility(View.VISIBLE);
+                say_ok_random_text_view.animate().alpha(1f);
+                intriguing.animate().alpha(0f);
+                speech = SpeechRecognizer.createSpeechRecognizer(this);
+                speech.setRecognitionListener(this);
+                promptSpeechInput();
+            }
+            break;
+
+        }
+    }
+
+    private void promptSpeechInput() {
+//        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+//                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+//        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+//                getString(R.string.speech_prompt));
+//        try {
+//            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+//        } catch (ActivityNotFoundException a) {
+//            Toast.makeText(getApplicationContext(),
+//                    getString(R.string.speech_not_supported),
+//                    Toast.LENGTH_SHORT).show();
+//        }
+        Intent recognizerIntent;
+        recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE,
+                "en");
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,
+                this.getPackageName());
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
+        progressBar1.setVisibility(View.VISIBLE);
+        speech.startListening(recognizerIntent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    for (String s : result) {
+//                        Log.d(TAG,"result ------->  "  + s + " <------- result");
+                        if (s.toLowerCase().contains("random")) {
+                            startActivity(new Intent(this, MainActivity.class));
+                        }
+                    }
+                }
+                break;
+            }
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (unbinder != null)
+            unbinder.unbind();
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+        Log.i(TAG, "onBeginningOfSpeech");
+    }
+
+    @Override
+    public void onBufferReceived(byte[] buffer) {
+        Log.i(TAG, "onBufferReceived: " + buffer);
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+        Log.i(TAG, "onEndOfSpeech");
+
+//        toggleButton.setChecked(false);
+    }
+
+    @Override
+    public void onError(int errorCode) {
+        String errorMessage = getErrorText(errorCode);
+        Log.d(TAG, "FAILED " + errorMessage);
+//        returnedText.setText(errorMessage);
+//        toggleButton.setChecked(false);
+    }
+
+    @Override
+    public void onEvent(int arg0, Bundle arg1) {
+        Log.i(TAG, "onEvent");
+    }
+
+    @Override
+    public void onPartialResults(Bundle arg0) {
+        Log.i(TAG, "onPartialResults");
+    }
+
+    @Override
+    public void onReadyForSpeech(Bundle arg0) {
+        Log.i(TAG, "onReadyForSpeech");
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+        Log.i(TAG, "onResults");
+        ArrayList<String> matches = results
+                .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        String text = "";
+        Boolean found=false;
+        if (matches != null) {
+            for (String result : matches) {
+                Log.d(TAG, "result ------->  " + result + " <------- result");
+                text += result + "\n";
+                if (result.toLowerCase().contains("random")) {
+                    progressBar1.setVisibility(View.GONE);
+                    speech.stopListening();
+                    intriguing.setChecked(false);
+                    intriguing.animate().alpha(1f);
+                    say_ok_random_text_view.setVisibility(View.INVISIBLE);
+                    intriguing.setVisibility(View.VISIBLE);
+                    found=true;
+                    startActivity(new Intent(this, MainActivity.class));
+                    break;
+                }
+            }
+
+        }
+        if(!found){
+            promptSpeechInput();
+
+        }
+
+//        returnedText.setText(text);
+    }
+
+    @Override
+    public void onRmsChanged(float rmsdB) {
+        Log.d(TAG, "onRmsChanged: " + rmsdB);
+//        progress.setProgress((int) rmsdB);
+    }
+}
